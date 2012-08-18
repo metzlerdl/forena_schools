10bdc1391aaf1cd26984ef0cec1b77242b7ff780
David Metzler <metzler.dl@gmail.com>
2012-08-18 13:18:49 -0700

    Added year labels to reports.

--- data_blocks/building/building_grade.sql
+++ data_blocks/building/building_grade.sql
@@ -1,4 +1,5 @@
 --ACCESS=PUBLIC
-SELECT b.*, g.abbrev grade_abbrev, g.name as grade_name from i_buildings b
+SELECT b.*, g.abbrev grade_abbrev, g.name as grade_name, y.label as year_label from i_buildings b
   JOIN i_grade_levels g ON g.grade_level = :grade_level
+  JOIN i_school_years y ON y.school_year = COALESCE(:school_year, i_school_year())
   WHERE bldg_id = :bldg_id
--- data_blocks/teacher/info.sql
+++ data_blocks/teacher/info.sql
@@ -4,10 +4,12 @@ SELECT
   p.person_id, 
   p.last_name, 
   p.first_name, 
-  b.name AS bldg_name
+  b.name AS bldg_name,
+  y.label AS year_label
 FROM p_staff s 
   JOIN p_people p ON p.person_id=s.person_id
   JOIN i_buildings b ON b.bldg_id = s.bldg_id
+  JOIN i_school_years y ON y.school_year = COALESCE(:school_year, i_school_year())
   WHERE s.staff_id = :staff_id 
   OR (p.login = :current_user AND :staff_id IS NULL)
 ORDER BY b.name
