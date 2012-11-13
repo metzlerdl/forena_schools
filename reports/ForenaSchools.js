d64504853ac6ea029fccf9cf841e174a0d8b9ab4
David Metzler <metzler.dl@gmail.com>
2012-11-13 11:28:47 -0800

    Changed scrolling mechanism.

--- reports/ForenaSchools.js
+++ reports/ForenaSchools.js
@@ -2,7 +2,8 @@ function ForenaSchools(context) {
 	var oTable = jQuery('table.pedagoggle-scores').dataTable({
 		"bPaginate": false, 
 		"bSort": true,
-		"sScrollX": "100%"
+		"sScrollX": "100%", 
+		"sScrollY": "25em"
 	}); 
 	new FixedColumns( oTable, {"iLeftColumns": 2,
 			"iLeftWidth": 250 });
