4e778549e6475bb442d983aa58ac6563edf71da7
David Metzler <metzler.dl@gmail.com>
2013-03-16 12:03:04 -0700

    UI Cleanup.

--- reports/ForenaSchools.js
+++ reports/ForenaSchools.js
@@ -6,17 +6,8 @@
         "bPaginate": false, 
         "bSort": true,
         "sScrollX": "100%",
-        "sScrollXInner": "100%",
-        "bScrollCollapse": false,
-        "sScrollY": "25em",
-        "fnDrawCallback": function ( oSettings ) {
-          /* Need to redo the counters if filtered or sorted */
-          if ( oSettings.bSorted || oSettings.bFiltered ) {
-            for ( var i=0, iLen=oSettings.aiDisplay.length ; i<iLen ; i++ ) {
-              this.fnUpdate( i+1, oSettings.aiDisplay[i], 0, false, false );
-            }
-          }
-        }
+        "sScrollY": "30em",
+        "bScrollCollapse": true
       }); 
       if (oTable) { 
         new FixedColumns( oTable, {"iLeftColumns": 1, "iLeftWidth": 200 });
