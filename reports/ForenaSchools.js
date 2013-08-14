7590278c3e61bda4c894eda0bd214084185d9e30
brandon martin <pie_liker@hotmail.com>
2013-08-14 15:09:39 -0700

    Re-organized many of the reports. Layouts have changed. most notably, table classes, and div classes replaceing grid with span for use with twitter bootstrap

--- reports/ForenaSchools.js
+++ reports/ForenaSchools.js
@@ -9,9 +9,43 @@
         "sScrollY": "500",
         "bScrollCollapse": true
       }); 
-    //  if (oTable) { 
-    //    new FixedColumns( oTable, {"iLeftColumns": 1, "iLeftWidth": 200 });
-    //  }
+      if (oTable) { 
+        new FixedColumns( oTable, {"iLeftColumns": 1, "iLeftWidth": 200 });
+      }
    }
+  };
+
+
+  Drupal.behaviors.peninsulaFeeds = {
+    attach: function(context, settings) {
+      console.log("well here we are!");
+      $("#websiteRSS-container").rss(
+          "http://www.psd401.net/index.php?format=feed&amp;type=rss",
+          {
+              limit: 2,
+              layoutTemplate: "{entries}",
+              entryTemplate: '<p><a href="{url}">{title}</a> {shortBody}</p><p class="sub">{date}</p>'
+          }
+      );
+
+      $("#techblogRSS-container").rss(
+          "http://techblog.psd401.net/?feed=rss2",
+          {
+              limit: 2,
+              layoutTemplate: "{entries}",
+              entryTemplate: '<p><a href="{url}">{title}</a> {shortBody}</p><p class="sub">{date}</p>'
+          }
+      );
+
+      $("#supRSS-container").rss(
+          "https://script.google.com/a/macros/krishagel.com/s/AKfycbxIvJLmswNQRctRQ3UrQqwad_1sOx_SIH5VrSugxXYU-ot2hsc/exec?action=timeline&q=psdsup",
+          {
+              limit: 2,
+              layoutTemplate: "{entries}",
+              entryTemplate: '<p><a href="{url}">{title}</a></p><p class="sub">{date}</p>'
+          }
+      );
+
+    }
   };
 })(jQuery);
