7513479d1ed60382025844b216bcb723962bc923
Brandon Martin <martinb@psd401.net>
2013-09-11 11:14:26 -0700

    Fixed Bootstrap and jQuery issues. Minimalized layout substatially.

--- reports/ForenaSchools.js
+++ reports/ForenaSchools.js
@@ -1,21 +1,5 @@
 (function ($) {
 
-  Drupal.behaviors.forenaSchools = {
-    attach: function (context, settings) {
-      var oTable = jQuery('table.pedagoggle-scores').dataTable({
-        "bPaginate": false, 
-        "bSort": true,
-        "sScrollX": "100%",
-        "sScrollY": "500",
-        "bScrollCollapse": true
-      }); 
-      if (oTable) { 
-        //new FixedColumns( oTable, {"iLeftColumns": 1, "iLeftWidth": 200 });
-      }
-   }
-  };
-
-
   Drupal.behaviors.peninsulaFeeds = {
     attach: function(context, settings) {
       console.log("well here we are!");
@@ -46,6 +30,32 @@
           }
       );
 
+      //Google Analytics
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-43721580-2']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
     }
+  };
+
+  Drupal.behaviors.forenaSchools = {
+    attach: function (context, settings) {
+      var oTable = jQuery('table.pedagoggle-scores').dataTable({
+        "bPaginate": false, 
+        "bSort": true,
+        "sScrollX": "100%",
+        "sScrollY": "500",
+        "bScrollCollapse": true
+      }); 
+      if (oTable) { 
+        //new FixedColumns( oTable, {"iLeftColumns": 1, "iLeftWidth": 200 });
+      }
+   }
   };
 })(jQuery);
