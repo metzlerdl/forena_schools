47b770c1b7efe56b34f72147ca1f3ed83c589b4d
David Metzler <metzlerd@gmail.com>
2014-11-18 08:34:40 -0800

    Updated styling.

--- reports/ForenaSchools.js
+++ reports/ForenaSchools.js
@@ -45,13 +45,15 @@
 
   Drupal.behaviors.forenaSchools = {
     attach: function (context, settings) {
-    var oTable = jQuery('table.pedagoggle-scores').dataTable({
+       var oTable = jQuery('table.pedagoggle-scores').dataTable({
         "bPaginate": false, 
         "bSort": true,
+        "bAutoWidth": false, 
         "sScrollX": "100%",
         "sScrollY": "500",  
         "bScrollCollapse": true
       }); 
+    
       if (oTable) { 
         //new FixedColumns( oTable, {"iLeftColumns": 1, "iLeftWidth": 200 });
       }
