4ccbc99f5825490b58cbbd44b3396baabdda14f7
David Metzler <metzlerd@gmail.com>
2014-11-22 12:48:00 -0800

    Changed to allow multiple selections on grad requirements.

--- data_blocks/search/standards.sql
+++ data_blocks/search/standards.sql
@@ -12,7 +12,7 @@ SELECT
   s.student_id, 
   s.grade_level, 
   (select max(date_taken) from a_assessments ac JOIN a_test_measures mc
-    ON mc.test_id = ac.test_id AND mc.grad_requirement = :grad_subject
+    ON mc.test_id = ac.test_id AND mc.grad_requirement in (:grad_subject)
     WHERE ac.person_id = p.person_id) last_taken 
 FROM
   p_students s JOIN p_people p ON s.person_id=p.person_id 
@@ -23,12 +23,15 @@ AND s.bldg_id = :bldg_id
 AND s.bldg_id IN (:security.buildings)
 AND s.grade_level in (:security.grades)
 AND s.grade_level BETWEEN :grade_level and CAST(COALESCE(:max_grade_level, :grade_level) AS int)
-AND 
-NOT EXISTS(
-  SELECT 1 FROM a_assessments a 
+AND  EXISTS(
+  SELECT subject from i_subjects WHERE subject in (:grad_subject)
+  EXCEPT 
+  (SELECT m.grad_requirement FROM a_assessments a 
     JOIN a_scores sc ON sc.assessment_id=a.assessment_id
     JOIN a_test_measures m ON sc.measure_id=m.measure_id
-  WHERE a.person_id = p.person_id AND m.grad_requirement = :grad_subject
+  WHERE a.person_id = p.person_id AND m.grad_requirement in (:grad_subject)
     AND sc.norm_score >= :norm_score
-  )
+  ))
 ORDER BY p.last_name, p.first_name
+--INFO
+type[grad_subject]=array
